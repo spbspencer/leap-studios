d1d4d2b92e13a99a2385f0c30bfe40ebc885ae64
Spencer Brown <spencerbrown@Spencers-MBP-2.lan>
2022-03-24 23:02:07 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,49 @@
+ScrollReveal({ reset: true });
+
+
+//hero section 
+ScrollReveal().reveal('.hero-content-container-1',{ delay: 200 });
+ScrollReveal().reveal('.hero-btn',{ delay: 400 });
+ScrollReveal().reveal('.hero-content-container-2',{ delay: 450 });
+
+
+
+
+//info section 
+
+ScrollReveal().reveal('.info-title-container',{ delay: 250 });
+ScrollReveal().reveal('.info-content-1',{ delay: 150 });
+ScrollReveal().reveal('.info-content-2',{ delay: 200 });
+ScrollReveal().reveal('.info-content-3',{ delay: 250 });
+ScrollReveal().reveal('.info-content-4',{ delay: 300 });
+
+
+
+
+//about-section 
+ScrollReveal().reveal('.about-content-container-1',{ delay: 200 });
+ScrollReveal().reveal('.about-content-container-2',{ delay: 350 });
+
+
+
+
+
+
+//card-section 
+
+// ScrollReveal().reveal('.card-title-container',{ delay: 350 });
+ScrollReveal().reveal('.card-content-1',{ delay: 150 });
+ScrollReveal().reveal('.card-content-2',{ delay: 200 });
+ScrollReveal().reveal('.card-content-3',{ delay: 250 });
+ScrollReveal().reveal('.card-content-4',{ delay: 300 });
+
+
+
+
+
+
+
+//faq section 
+ScrollReveal().reveal('.faq-content-container-1',{ delay: 350 });
+ScrollReveal().reveal('.faq-content-container-2',{ delay: 450 });
+ScrollReveal().reveal('.faq-content-container-3',{ delay: 550 });
